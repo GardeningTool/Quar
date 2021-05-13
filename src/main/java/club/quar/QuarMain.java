f3fca77378798e6f8f3a6851a03cb2bb2f8e4e3f
Gardening_Tool <54986171+GardeningTool@users.noreply.github.com>
2021-05-12 21:19:41 -0500

    Added a main class and plugin.yml so Quar can be used without being shaded in

---
+++ src/main/java/club/quar/QuarMain.java
@@ -0,0 +1,10 @@
+package club.quar;
+
+import club.quar.plugin.QuarPlugin;
+
+/*
+Only exists so Quar can be used as a standalone plugin.
+Through this, Quar doesn't have to be shaded into multiple plugins
+ */
+public class QuarMain extends QuarPlugin {
+}
