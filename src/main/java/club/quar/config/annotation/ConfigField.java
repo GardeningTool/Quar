a7a7e82bdf9b4421134be804d755dc94c08fcd41
Numzskull <23282729+Numzskull@users.noreply.github.com>
2021-08-28 02:28:17 -0400

    Fixed ConfigField Annotation not being specifically specified for fields

--- src/main/java/club/quar/config/annotation/ConfigField.java
+++ src/main/java/club/quar/config/annotation/ConfigField.java
@@ -1,9 +1,12 @@
 package club.quar.config.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface ConfigField {
 
     /**
